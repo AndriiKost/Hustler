89f9956e543b989b6682842fc636d340425ca7f2
Andrii Kost <andriikost@yahoo.com>
2018-02-12 09:11:32 -0600

    Initial Commit

---
+++ Hustler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hustler
+//
+//  Created by Andrii Kost on 2/12/18.
+//  Copyright © 2018 Andrii Kost. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
